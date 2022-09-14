aa7cb36abd17e9aff98239c55ce42516b2503d5e
Luke Billings <lukecbillings@gmail.com>
2022-09-14 15:34:52 +0100

    added tree class

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Tree {
+public class Tree {
+    public static void main(String[] args) {
+        // print a tree
+        System.out.println("Hi there");
+        System.out.println("We are planting a tree today!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
 }
